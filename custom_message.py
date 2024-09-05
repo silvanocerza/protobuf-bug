a54ca919eea97bb2757c7bb8f2fc8d47ce9eb623
Silvano Cerza <silvanocerza@gmail.com>
2024-09-05 16:58:43 +0200

    Minimise

--- custom_message.py
+++ custom_message.py
@@ -1,15 +1,12 @@
 from typing import MutableSequence
 
 import proto
-from google.protobuf import struct_pb2  # type: ignore
 
 __protobuf__ = proto.module(
     package="custom_package",
     manifest={
         "Chat",
         "CustomMessage",
-        "Type",
-        "Schema",
     },
 )
 
@@ -30,63 +27,6 @@ class CustomMessage(proto.Message):
     description: str = proto.Field(
         proto.STRING,
         number=2,
-    )
-    parameters: "Schema" = proto.Field(
-        proto.MESSAGE,
-        number=3,
-        message="Schema",
-    )
-
-
-class Type(proto.Enum):
-    TYPE_UNSPECIFIED = 0
-    STRING = 1
-    NUMBER = 2
-    INTEGER = 3
-    BOOLEAN = 4
-    ARRAY = 5
-    OBJECT = 6
-
-
-class Schema(proto.Message):
-    type_: "Type" = proto.Field(
-        proto.ENUM,
-        number=1,
-        enum="Type",
-    )
-    format_: str = proto.Field(
-        proto.STRING,
-        number=7,
-    )
-    title: str = proto.Field(
-        proto.STRING,
-        number=24,
-    )
-    description: str = proto.Field(
-        proto.STRING,
-        number=8,
-    )
-    nullable: bool = proto.Field(
-        proto.BOOL,
-        number=6,
-    )
-    default: struct_pb2.Value = proto.Field(
-        proto.MESSAGE,
-        number=23,
-        message=struct_pb2.Value,
-    )
-    items: "Schema" = proto.Field(
-        proto.MESSAGE,
-        number=2,
-        message="Schema",
-    )
-    min_items: int = proto.Field(
-        proto.INT64,
-        number=21,
-    )
-    max_items: int = proto.Field(
-        proto.INT64,
-        number=22,
     )
 
 
--- test.py
+++ test.py
@@ -2,12 +2,6 @@ import custom_message
 
 message = custom_message.CustomMessage(
     name="name",
-    description="description",
-    parameters=custom_message.Schema(
-        type_=custom_message.Type.STRING,
-        format_="format",
-        title="title",
-    ),
 )
 
 chat = custom_message.Chat(messages=[message])
@@ -15,7 +9,6 @@ chat = custom_message.Chat(messages=[message])
 data = type(chat).to_dict(
     chat,
     including_default_value_fields=False,
-    use_integers_for_enums=False,
 )
 
 print(data)
@@ -23,8 +16,6 @@ assert data == {
     "messages": [
         {
             "name": "name",
-            "description": "description",
-            "parameters": {"type_": "STRING", "format_": "format", "title": "title"},
         }
     ]
 }
